2727da1a87a7305702567beccec27d60a883af96
micropuma <micropuma@163.com>
2026-01-12 15:06:37 +0000

    [test] TTIR to TT-Metal test case

--- tutorial/Pipeline/test-simple.sh
+++ tutorial/Pipeline/test-simple.sh
@@ -4,9 +4,7 @@ ttmlir-opt \
     --mlir-print-ir-after-all \
     2>&1 | tee ttn.log
 
-ttmlir-opt \
-    --ttir-to-ttmetal-pipeline \
-    ../../test/ttmlir/Dialect/TTNN/simple_gelu_bw.mlir \
-    --mlir-print-ir-after-all \
-    2>&1 | tee ttmetal.log
+ttmlir-opt --ttir-to-ttmetal-pipeline \
+    test-matmul.mlir
+
 
